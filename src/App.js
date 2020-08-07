1d18105800661ae8261f6f3fbe34fa2dcbfce73e
Anna <anna_agabekyan_anna@mail.ru>
2020-08-07 18:52:32 +0400

    implementation navbar, banner

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import React from 'react';
 import './App.css';
 import requests from './requests';
 import Row from './Row';
+import Banner from './Banner';
+import Nav from "./Nav";
 
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
+      <Nav />
+      <Banner />
       <Row
         title="NETFLIX ORIGINALS"
         fetchUrl={requests.fetchNetflixOriginals}
